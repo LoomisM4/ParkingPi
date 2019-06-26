f45cbc2c0a5ade93c1e75afe3011962c87085621
LoomisM4 <github@marcel-wettach.com>
2019-06-26 11:10:45 +0200

    RFID-Sensor will now be used

--- ParkingPi.py
+++ ParkingPi.py
@@ -1,40 +1,54 @@
-from ParkingSpot import ParkingSpot
+from objects.ParkingSpot import ParkingSpot
+from sensors.rfid.RfidReader import RfidReader
 from sensors.ultrasonic.UltrasonicSensor import UltrasonicSensor
 from mqtt.MQTT import MQTT
 import time
-#import pydevd_pycharm
-#pydevd_pycharm.settrace('192.168.178.20', port=9000, stdoutToServer=True, stderrToServer=True)
 
 TRIGGER_HEIGHT = 60
 TIMEOUT = 30
+
+json = "spot_id: {}, user_id: {}, free: {}, since: {}"
 
 print("Welcome to ParkingPi.")
 print("This Software runs in the background and communicates with a MQTT-Broker. So never mind missing outputs.")
 
 # set up the sensors
-sensor = UltrasonicSensor(trigger_pin=18, echo_pin=24)
+sensor = UltrasonicSensor(trigger_pin=12, echo_pin=18)
+rfid = RfidReader()
 
 # set up the parking spots
-spot1 = ParkingSpot(1, sensor, None)
+spot1 = ParkingSpot(1, sensor, rfid)
 
 # add the spots to an array
 spots = [spot1]
 
-while True:
-    mqtt = MQTT()
+mqtt = MQTT()
 
+
+def publish_json(spot_id, user_id, free, timestamp):
+    temp = json.format(spot_id, user_id, free, timestamp)
+    mqtt.publish("{" + temp + "}")
+
+
+def get_current_timestamp():
+    return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+
+while True:
     for spot in spots:
         distance = spot.ULTRASONIC_SENSOR.measure_distance()
 
-        if distance <= TRIGGER_HEIGHT and spot.FREE:
+        if distance <= TRIGGER_HEIGHT and spot.free:
             # spot is now in use
-            spot.FREE = False
-            # TODO RFID
-            mqtt.publish("In Benutzung: " + str(spot.ID))
-        elif distance > TRIGGER_HEIGHT and not spot.FREE:
+            tmstmp = get_current_timestamp()
+            spot.free = False
+            uid = spot.RFID_SENSOR.read_card()
+            publish_json(spot.ID, uid, spot.free, tmstmp)
+        elif distance > TRIGGER_HEIGHT and not spot.free:
             # spot is free now
-            spot.FREE = True
-            mqtt.publish("Wieder frei: " + str(spot.ID))
+            tmstmp = get_current_timestamp()
+            spot.free = True
+            publish_json(spot.ID, None, spot.free, tmstmp)
         else:
             # spot is either still in use or still not in use
             pass
